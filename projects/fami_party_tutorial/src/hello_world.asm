fc11bc764d1c9dea406eaf62ff57689165ed2b89
Ryan ODell <ryanodell2013@gmail.com>
2024-04-08 10:24:37 -0700

    added sprites and background tiles

--- projects/fami_party_tutorial/src/hello_world.asm
+++ projects/fami_party_tutorial/src/hello_world.asm
@@ -11,6 +11,9 @@
   STA OAMADDR
   LDA #$02
   STA OAMDMA
+  LDA #$00
+  STA $2005
+  STA $2005
   RTI
 .endproc
 
@@ -28,7 +31,7 @@ load_palletes:
   LDA palettes, X
   STA PPUDATA
   INX
-  CPX #$04
+  CPX #$20
   BNE load_palletes
 
   ; write sprite data
@@ -37,8 +40,138 @@ load_sprites:
   LDA sprites, X
   STA $0200, X
   INX
-  CPX #$04
+  CPX #$10
   BNE load_sprites
+
+	; write nametables
+	; big stars first
+	LDA PPUSTATUS
+	LDA #$20
+	STA PPUADDR
+	LDA #$6b
+	STA PPUADDR
+	LDX #$2f
+	STX PPUDATA
+
+	LDA PPUSTATUS
+	LDA #$21
+	STA PPUADDR
+	LDA #$57
+	STA PPUADDR
+	STX PPUDATA
+
+	LDA PPUSTATUS
+	LDA #$22
+	STA PPUADDR
+	LDA #$23
+	STA PPUADDR
+	STX PPUDATA
+
+	LDA PPUSTATUS
+	LDA #$23
+	STA PPUADDR
+	LDA #$52
+	STA PPUADDR
+	STX PPUDATA
+
+	; next, small star 1
+	LDA PPUSTATUS
+	LDA #$20
+	STA PPUADDR
+	LDA #$74
+	STA PPUADDR
+	LDX #$2d
+	STX PPUDATA
+
+	LDA PPUSTATUS
+	LDA #$21
+	STA PPUADDR
+	LDA #$43
+	STA PPUADDR
+	STX PPUDATA
+
+	LDA PPUSTATUS
+	LDA #$21
+	STA PPUADDR
+	LDA #$5d
+	STA PPUADDR
+	STX PPUDATA
+
+	LDA PPUSTATUS
+	LDA #$21
+	STA PPUADDR
+	LDA #$73
+	STA PPUADDR
+	STX PPUDATA
+
+	LDA PPUSTATUS
+	LDA #$22
+	STA PPUADDR
+	LDA #$2f
+	STA PPUADDR
+	STX PPUDATA
+
+	LDA PPUSTATUS
+	LDA #$22
+	STA PPUADDR
+	LDA #$f7
+	STA PPUADDR
+	STX PPUDATA
+
+	; finally, small star 2
+	LDA PPUSTATUS
+	LDA #$20
+	STA PPUADDR
+	LDA #$f1
+	STA PPUADDR
+	LDX #$2e
+	STX PPUDATA
+
+	LDA PPUSTATUS
+	LDA #$21
+	STA PPUADDR
+	LDA #$a8
+	STA PPUADDR
+	STX PPUDATA
+
+	LDA PPUSTATUS
+	LDA #$22
+	STA PPUADDR
+	LDA #$7a
+	STA PPUADDR
+	STX PPUDATA
+
+	LDA PPUSTATUS
+	LDA #$23
+	STA PPUADDR
+	LDA #$44
+	STA PPUADDR
+	STX PPUDATA
+
+	LDA PPUSTATUS
+	LDA #$23
+	STA PPUADDR
+	LDA #$7c
+	STA PPUADDR
+	STX PPUDATA
+
+	; finally, attribute table
+	LDA PPUSTATUS
+	LDA #$23
+	STA PPUADDR
+	LDA #$c2
+	STA PPUADDR
+	LDA #%01000000
+	STA PPUDATA
+
+	LDA PPUSTATUS
+	LDA #$23
+	STA PPUADDR
+	LDA #$e0
+	STA PPUADDR
+	LDA #%00001100
+	STA PPUDATA
+
 vblankwait:       ; wait for another vblank before continuing
   BIT PPUSTATUS
   BPL vblankwait
@@ -53,9 +186,20 @@ forever:
 
 .segment "RODATA"
 palettes:
-.byte $29, $19, $09, $0f
+.byte $0f, $12, $23, $27
+.byte $0f, $2b, $3c, $39
+.byte $0f, $0c, $07, $13
+.byte $0f, $19, $09, $29
+
+.byte $0f, $2d, $10, $15
+.byte $0f, $19, $09, $29
+.byte $0f, $19, $09, $29
+.byte $0f, $19, $09, $29
 sprites:
 .byte $70, $05, $00, $80
+.byte $70, $06, $00, $88
+.byte $78, $07, $00, $80
+.byte $78, $08, $00, $88
 
 .segment "VECTORS"
 .addr nmi_handler, reset_handler, irq_handler
