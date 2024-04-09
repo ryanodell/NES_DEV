0d9226c0bbe212cc083f142ec17f342987d6499a
Ryan ODell <ryanodell2013@gmail.com>
2024-04-09 02:42:41 -0700

    added code for moving ship back and forth

--- projects/fami_party_tutorial/src/hello_world.asm
+++ projects/fami_party_tutorial/src/hello_world.asm
@@ -11,6 +11,11 @@
   STA OAMADDR
   LDA #$02
   STA OAMDMA
+
+  ; update tiles AFTER DMA transfer
+  JSR update_player
+  JSR draw_player
+
   LDA #$00
   STA $2005
   STA $2005
@@ -18,6 +23,121 @@
 .endproc
 
 .import reset_handler
+
+.proc update_player
+  PHP
+  PHA
+  TXA
+  PHA
+  TYA
+  PHA
+
+  LDA player_x
+  CMP #$e0
+  BCC not_at_right_edge
+  ; if BCC is not taken, we are greater than $e0
+  LDA #$00
+  STA player_dir    ; start moving left
+  JMP direction_set ; we already chose a direction,
+                    ; so we can skip the left side check
+not_at_right_edge:
+  LDA player_x
+  CMP #$10
+  BCS direction_set
+  ; if BCS not taken, we are less than $10
+  LDA #$01
+  STA player_dir   ; start moving right
+direction_set:
+  ; now, actually update player_x
+  LDA player_dir
+  CMP #$01
+  BEQ move_right
+  ; if player_dir minus $01 is not zero,
+  ; that means player_dir was $00 and
+  ; we need to move left
+  DEC player_x
+  JMP exit_subroutine
+move_right:
+  INC player_x
+exit_subroutine:
+  ; all done, clean up and return
+  PLA
+  TAY
+  PLA
+  TAX
+  PLA
+  PLP
+  RTS
+.endproc
+
+.proc draw_player
+  ;Save the registers
+  PHP
+  PHA
+  TXA
+  PHA
+  TYA
+  PHA
+  ; write player ship tile numbers
+  LDA #$05
+  STA $0201
+  LDA #$06
+  STA $0205
+  LDA #$07
+  STA $0209
+  LDA #$08
+  STA $020d
+
+  ; write player ship tile attributes
+  ; use palette 0
+  LDA #$00
+  STA $0202
+  STA $0206
+  STA $020a
+  STA $020e
+
+  ; store tile locations
+  ; top left tile:
+  LDA player_y
+  STA $0200
+  LDA player_x
+  STA $0203
+
+  ; top right tile (x + 8):
+  LDA player_y
+  STA $0204
+  LDA player_x
+  CLC
+  ADC #$08
+  STA $0207
+
+  ; bottom left tile (y + 8):
+  LDA player_y
+  CLC
+  ADC #$08
+  STA $0208
+  LDA player_x
+  STA $020b
+
+  ; bottom right tile (x + 8, y + 8)
+  LDA player_y
+  CLC
+  ADC #$08
+  STA $020c
+  LDA player_x
+  CLC
+  ADC #$08
+  STA $020f
+
+  ; restore registers and return
+  PLA
+  TAY
+  PLA
+  TAX
+  PLA
+  PLP
+  RTS
+.endproc
 
 .export main
 .proc main
@@ -35,13 +155,13 @@ load_palletes:
   BNE load_palletes
 
   ; write sprite data
-  LDX #$00
-load_sprites:
-  LDA sprites, X
-  STA $0200, X
-  INX
-  CPX #$10
-  BNE load_sprites
+;   LDX #$00
+; load_sprites:
+;   LDA sprites, X
+;   STA $0200, X
+;   INX
+;   CPX #$10
+;   BNE load_sprites
 
 	; write nametables
 	; big stars first
@@ -183,6 +303,12 @@ vblankwait:       ; wait for another vblank before continuing
 forever:
   JMP forever
 .endproc
+
+.segment "ZEROPAGE"
+player_x: .res 1
+player_y: .res 1
+player_dir: .res 1
+.exportzp player_x, player_y
 
 .segment "RODATA"
 palettes:
