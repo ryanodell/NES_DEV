e3e2ff7add9e6ee2ac818128a88e67e545659cf4
Ryan ODell <ryanodell2013@gmail.com>
2024-04-10 09:52:26 -0700

    commented original update and replaced with movement code

--- projects/fami_party_tutorial/src/hello_world.asm
+++ projects/fami_party_tutorial/src/hello_world.asm
@@ -45,50 +45,88 @@ set_scroll_positions:
 .import reset_handler
 
 .proc update_player
-  PHP
-  PHA
+  PHP  ; Start by saving registers,
+  PHA  ; as usual.
   TXA
   PHA
   TYA
   PHA
 
-  LDA player_x
-  CMP #$e0
-  BCC not_at_right_edge
-  ; if BCC is not taken, we are greater than $e0
-  LDA #$00
-  STA player_dir    ; start moving left
-  JMP direction_set ; we already chose a direction,
-                    ; so we can skip the left side check
-not_at_right_edge:
-  LDA player_x
-  CMP #$10
-  BCS direction_set
-  ; if BCS not taken, we are less than $10
-  LDA #$01
-  STA player_dir   ; start moving right
-direction_set:
-  ; now, actually update player_x
-  LDA player_dir
-  CMP #$01
-  BEQ move_right
-  ; if player_dir minus $01 is not zero,
-  ; that means player_dir was $00 and
-  ; we need to move left
-  DEC player_x
-  JMP exit_subroutine
-move_right:
+  LDA pad1        ; Load button presses
+  AND #BTN_LEFT   ; Filter out all but Left
+  BEQ check_right ; If result is zero, left not pressed
+  DEC player_x  ; If the branch is not taken, move player left
+check_right:
+  LDA pad1
+  AND #BTN_RIGHT
+  BEQ check_up
   INC player_x
-exit_subroutine:
-  ; all done, clean up and return
-  PLA
-  TAY
+check_up:
+  LDA pad1
+  AND #BTN_UP
+  BEQ check_down
+  DEC player_y
+check_down:
+  LDA pad1
+  AND #BTN_DOWN
+  BEQ done_checking
+  INC player_y
+done_checking:
+  PLA ; Done with updates, restore registers
+  TAY ; and return to where we called this
   PLA
   TAX
   PLA
   PLP
   RTS
 .endproc
+
+;DO NOT REMOVE THIS, THIS IS A GOOD EXAMPLE FOR BOUND CHECKING
+; .proc update_player
+;   PHP
+;   PHA
+;   TXA
+;   PHA
+;   TYA
+;   PHA
+
+;   LDA player_x
+;   CMP #$e0
+;   BCC not_at_right_edge
+;   ; if BCC is not taken, we are greater than $e0
+;   LDA #$00
+;   STA player_dir    ; start moving left
+;   JMP direction_set ; we already chose a direction,
+;                     ; so we can skip the left side check
+; not_at_right_edge:
+;   LDA player_x
+;   CMP #$10
+;   BCS direction_set
+;   ; if BCS not taken, we are less than $10
+;   LDA #$01
+;   STA player_dir   ; start moving right
+; direction_set:
+;   ; now, actually update player_x
+;   LDA player_dir
+;   CMP #$01
+;   BEQ move_right
+;   ; if player_dir minus $01 is not zero,
+;   ; that means player_dir was $00 and
+;   ; we need to move left
+;   DEC player_x
+;   JMP exit_subroutine
+; move_right:
+;   INC player_x
+; exit_subroutine:
+;   ; all done, clean up and return
+;   PLA
+;   TAY
+;   PLA
+;   TAX
+;   PLA
+;   PLP
+;   RTS
+; .endproc
 
 .proc draw_player
   ;Save the registers
