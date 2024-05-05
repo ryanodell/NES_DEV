5e2df16d8adc9d4f7bfcc33fd9dece92a2c84004
Ryan ODell <ryanodell2013@gmail.com>
2024-05-05 11:33:16 -0700

    attempt01 scaffolding

--- projects/attempt_01/src/main.asm
+++ projects/attempt_01/src/main.asm
@@ -1,1 +1,34 @@
-;tmp
+.include "include/constants.inc"
+.include "include/header.inc"
+
+.segment "CODE"
+.import reset_handler
+
+.proc irq_handler
+    RTI
+.endproc
+
+.proc nmi_handler
+  RTI
+.endproc
+
+.export main
+.proc main
+  LDX $2002
+  LDX #$3f
+  STX $2006
+  LDX #$00
+  STX $2006
+  LDA #$29
+  STA $2007
+  LDA #%00011110
+  STA $2001
+forever:
+  JMP forever
+.endproc
+
+.segment "VECTORS"
+.addr nmi_handler, reset_handler, irq_handler
+
+.segment "CHR"
+.res 8192
