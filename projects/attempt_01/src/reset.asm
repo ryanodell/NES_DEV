d4fe87ba9b4a8223bc9939fe726169dba10cdada
Ryan ODell <ryanodell2013@gmail.com>
2024-05-06 10:49:55 -0700

    clearing oam, added some test nss files

--- projects/attempt_01/src/reset.asm
+++ projects/attempt_01/src/reset.asm
@@ -18,6 +18,15 @@
 vblankwait:
   BIT $2002
   BPL vblankwait
+  LDX #$00
+  LDA #$ff
+clear_oam:
+  STA $0200, X ; set sprite y-positions off the screen
+  INX
+  INX
+  INX
+  INX
+  BNE clear_oam
 vblankwait2:
   BIT $2002
   BPL vblankwait2
