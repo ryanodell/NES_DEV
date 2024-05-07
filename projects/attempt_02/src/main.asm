1172029028310f7f26210918986a0b5db23b2788
Ryan ODell <ryanodell2013@gmail.com>
2024-05-07 12:55:11 -0700

    fixed build issues

--- projects/attempt_02/src/main.asm
+++ projects/attempt_02/src/main.asm
@@ -13,14 +13,28 @@
 
 .segment "CODE"
 .export main
-.proc main:
-
+.proc main
+  LDX PPUSTATUS
+  LDX #$3f
+  STX PPUADDR
+  LDX #$00
+  STX PPUADDR
+  LDA #$29
+  STA PPUDATA
+  LDA #%00011110
+  STA PPUMASK
+forever:
+  JMP forever
 .endproc
 
-.proc nmi_handler:
+.proc nmi_handler
   RTI
 .endproc
 
 .proc irq_handler
   RTI
 .endproc
+
+; Just reserve it for now
+.segment "CHR"
+.res 8192
