edcadb29c620796a92dd690034788f988a79f1ba
Ryan ODell <ryanodell2013@gmail.com>
2024-05-21 09:56:36 -0700

    Added further documentation

--- projects/attempt_02/src/enemies.asm
+++ projects/attempt_02/src/enemies.asm
@@ -6,120 +6,119 @@
 .segment "CODE"
 .export draw_enemy
 .proc draw_enemy
-SaveRegisters
-  LDX current_enemy
-  LDA enemy_flags, X
-  AND #%10000000          ; Highest bit is the 'Active' flag
-  BNE continue
-  JMP done
+  SaveRegisters                ; Save the state of the CPU registers
+
+  LDX current_enemy            ; Load the current enemy index into X
+  LDA enemy_flags, X           ; Load the enemy's flags using the X index
+  AND #%10000000               ; Check if the highest bit (Active flag) is set
+  BNE continue                 ; If the Active flag is set, branch to continue
+  JMP done                     ; If not, jump to done (skip drawing this enemy)
 
 continue:
-  LDA #$10
-  LDX current_enemy
-  BEQ oam_address_found
+  LDA #$10                     ; Initialize A with the base offset for OAM
+  LDX current_enemy            ; Reload current enemy index into X
+  BEQ oam_address_found        ; If the current enemy index is zero, branch to oam_address_found
 
 find_address:
-  CLC
-  ADC #$10
-  DEX
-  BNE find_address
+  CLC                          ; Clear the carry flag before addition
+  ADC #$10                     ; Add 16 to A (next OAM address block)
+  DEX                          ; Decrement X
+  BNE find_address             ; If X is not zero, repeat the loop
 
 oam_address_found:
-  LDX current_enemy
-  TAY ; use Y to hold OAM address offset
-  ; Find the current enemy's type and
-  ; store it for later use. The enemy type
-  ; is in bits 0-2 of enemy_flags.
-  LDA enemy_flags, X
-  AND #%00000111
-  STA current_enemy_type
-  ; enemy top-left
-  LDA enemy_y_pos, X
-  STA $0200, Y
-  INY
-  LDX current_enemy_type
-  LDA enemy_top_lefts, X
-  STA $0200, Y
-  INY
-  LDA enemy_palettes, X
-  STA $0200, Y
-  INY
-  LDX current_enemy
-  LDA enemy_x_pos, X
-  STA $0200, Y
-  INY
+  LDX current_enemy            ; Reload current enemy index into X
+  TAY                          ; Transfer the OAM address offset to Y
+  LDA enemy_flags, X           ; Load the enemy's flags using the X index
+  AND #%00000111               ; Mask out the bits to get the enemy type (bits 0-2)
+  STA current_enemy_type       ; Store the enemy type in current_enemy_type
 
-  ; enemy top-right
-  LDA enemy_y_pos, X
-  STA $0200, Y
-  INY
-  LDX current_enemy_type
-  LDA enemy_top_rights, X
-  STA $0200, Y
-  INY
-  LDA enemy_palettes, X
-  STA $0200, Y
-  INY
-  LDX current_enemy
-  LDA enemy_x_pos, X
-  CLC
-  ADC #$08
-  STA $0200, Y
-  INY
+  ; Draw enemy top-left
+  LDA enemy_y_pos, X           ; Load the enemy's Y position using X index
+  STA $0200, Y                 ; Store Y position in OAM
+  INY                          ; Increment Y for next OAM byte
+  LDX current_enemy_type       ; Load the enemy type into X
+  LDA enemy_top_lefts, X       ; Load the top-left tile ID for the enemy type
+  STA $0200, Y                 ; Store tile ID in OAM
+  INY                          ; Increment Y for next OAM byte
+  LDA enemy_palettes, X        ; Load the palette for the enemy type
+  STA $0200, Y                 ; Store the palette in OAM
+  INY                          ; Increment Y for next OAM byte
+  LDX current_enemy            ; Reload current enemy index into X
+  LDA enemy_x_pos, X           ; Load the enemy's X position using X index
+  STA $0200, Y                 ; Store X position in OAM
+  INY                          ; Increment Y for next OAM byte
 
-  ; enemy bottom-left
-  LDA enemy_y_pos, X
-  CLC
-  ADC #$08
-  STA $0200, Y
-  INY
-  LDX current_enemy_type
-  LDA enemy_bottom_lefts, X
-  STA $0200,Y
-  INY
-  LDA enemy_palettes, X
-  STA $0200, Y
-  INY
-  LDX current_enemy
-  LDA enemy_x_pos, X
-  STA $0200, Y
-  INY
+  ; Draw enemy top-right
+  LDA enemy_y_pos, X           ; Load the enemy's Y position again
+  STA $0200, Y                 ; Store Y position in OAM
+  INY                          ; Increment Y for next OAM byte
+  LDX current_enemy_type       ; Reload enemy type into X
+  LDA enemy_top_rights, X      ; Load the top-right tile ID for the enemy type
+  STA $0200, Y                 ; Store tile ID in OAM
+  INY                          ; Increment Y for next OAM byte
+  LDA enemy_palettes, X        ; Load the palette again
+  STA $0200, Y                 ; Store the palette in OAM
+  INY                          ; Increment Y for next OAM byte
+  LDX current_enemy            ; Reload current enemy index into X
+  LDA enemy_x_pos, X           ; Load the enemy's X position again
+  CLC                          ; Clear the carry flag before addition
+  ADC #$08                     ; Add 8 to X position (for the right side)
+  STA $0200, Y                 ; Store X position in OAM
+  INY                          ; Increment Y for next OAM byte
 
-  ; enemy bottom-right
-  LDA enemy_y_pos, X
-  CLC
-  ADC #$08
-  STA $0200, Y
-  INY
-  LDX current_enemy_type
-  LDA enemy_bottom_rights, X
-  STA $0200,Y
-  INY
-  LDA enemy_palettes, X
-  STA $0200,Y
-  INY
-  LDX current_enemy
-  LDA enemy_x_pos, X
-  CLC
-  ADC #$08
-  STA $0200, Y
+  ; Draw enemy bottom-left
+  LDA enemy_y_pos, X           ; Load the enemy's Y position again
+  CLC                          ; Clear the carry flag before addition
+  ADC #$08                     ; Add 8 to Y position (for the bottom side)
+  STA $0200, Y                 ; Store Y position in OAM
+  INY                          ; Increment Y for next OAM byte
+  LDX current_enemy_type       ; Reload enemy type into X
+  LDA enemy_bottom_lefts, X    ; Load the bottom-left tile ID for the enemy type
+  STA $0200, Y                 ; Store tile ID in OAM
+  INY                          ; Increment Y for next OAM byte
+  LDA enemy_palettes, X        ; Load the palette again
+  STA $0200, Y                 ; Store the palette in OAM
+  INY                          ; Increment Y for next OAM byte
+  LDX current_enemy            ; Reload current enemy index into X
+  LDA enemy_x_pos, X           ; Load the enemy's X position again
+  STA $0200, Y                 ; Store X position in OAM
+  INY                          ; Increment Y for next OAM byte
+
+  ; Draw enemy bottom-right
+  LDA enemy_y_pos, X           ; Load the enemy's Y position again
+  CLC                          ; Clear the carry flag before addition
+  ADC #$08                     ; Add 8 to Y position (for the bottom side)
+  STA $0200, Y                 ; Store Y position in OAM
+  INY                          ; Increment Y for next OAM byte
+  LDX current_enemy_type       ; Reload enemy type into X
+  LDA enemy_bottom_rights, X   ; Load the bottom-right tile ID for the enemy type
+  STA $0200, Y                 ; Store tile ID in OAM
+  INY                          ; Increment Y for next OAM byte
+  LDA enemy_palettes, X        ; Load the palette again
+  STA $0200, Y                 ; Store the palette in OAM
+  INY                          ; Increment Y for next OAM byte
+  LDX current_enemy            ; Reload current enemy index into X
+  LDA enemy_x_pos, X           ; Load the enemy's X position again
+  CLC                          ; Clear the carry flag before addition
+  ADC #$08                     ; Add 8 to X position (for the right side)
+  STA $0200, Y                 ; Store X position in OAM
 
 done:
-  RestoreRegisters
-  RTS
+  RestoreRegisters             ; Restore the state of the CPU registers
+  RTS                          ; Return from subroutine
 .endproc
 
 
 .segment "RODATA"
 
 enemy_top_lefts:
-.byte $09, $0d
+.byte $09, $0d                 ; Tile IDs for the top-left of enemies
 enemy_top_rights:
-.byte $0b, $0e
+.byte $0b, $0e                 ; Tile IDs for the top-right of enemies
 enemy_bottom_lefts:
-.byte $0a, $0f
+.byte $0a, $0f                 ; Tile IDs for the bottom-left of enemies
 enemy_bottom_rights:
-.byte $0c, $10
+.byte $0c, $10                 ; Tile IDs for the bottom-right of enemies
 
 enemy_palettes:
-.byte $01, $02
+.byte $01, $02                 ; Palettes for enemies
